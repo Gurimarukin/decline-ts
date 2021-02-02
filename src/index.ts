539a428205a0e1d7ab6195a373c20a5758319c55
Gurimarukin <naxel@laposte.net>
2021-02-02 18:21:56 +0100

    Add codecToDecode

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+import { pipe } from 'fp-ts/function'
+import * as D from 'io-ts/Decoder'
+
+import { Either } from './utils/fp'
+import { ValidatedNea } from './ValidatedNea'
+
 export * from './Command'
 export * from './Opts'
+
+export const codecToDecode = <I, A>(codec: D.Decoder<I, A>) => (u: I): ValidatedNea<string, A> =>
+  pipe(codec.decode(u), Either.mapLeft(D.draw), ValidatedNea.fromEither)
